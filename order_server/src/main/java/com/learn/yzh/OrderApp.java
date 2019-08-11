d1b40849305bf30c32cb6410c7df456853d2a7a4
yangzhanghui <m15711383458@163.com>
2019-08-11 23:42:30 +0800

    sss

---
+++ order_server/src/main/java/com/learn/yzh/OrderApp.java
@@ -0,0 +1,12 @@
+package com.learn.yzh;
+
+/**
+ * Order Server
+ *
+ */
+public class OrderApp {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
