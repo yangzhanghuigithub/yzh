a1453cfb59a4f5e399c1aae4de74f0027dfdd4a0
yangzhanghui <m15711383458@163.com>
2019-08-22 17:46:43 +0800

    提交

---
+++ job_server/src/main/java/com/learn/yzh/JobApp.java
@@ -0,0 +1,43 @@
+package com.learn.yzh;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Job Server
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableScheduling
+@MapperScan("com.yj.learn.mapper")
+@ComponentScan(basePackages = {"com.yj.learn"})
+@EnableFeignClients //启用feign进行远程调用
+public class JobApp {
+    public static void main( String[] args ) {
+        SpringApplication.run(JobApp.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RequestMapping(value = "/test")
+    public String test(){
+        System.out.print("this is server_job");
+        return "this is server_job";
+    }
+}
